8380785a6dc91d8c88911b76cbbae30b379003ff
Sean Tang <work.seantang@gmail.com>
2023-04-23 16:59:32 -0700

    updated versions setup.

--- chat/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<chat_box_name>\w+)/$", consumers.ChatRoomConsumer.as_asgi()), # regex of w+ matches any word and /$ matches end of word, so no extended URl
-]
