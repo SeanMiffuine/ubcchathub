f411551f4455ae46605d79953bbdc79f8826f463
Sean Tang <work.seantang@gmail.com>
2023-05-03 23:32:15 -0700

    websocket chat send and recieve for multiple users

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,6 +120,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 ASGI_APPLICATION = "mysite.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
