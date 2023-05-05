006c84f8d45d168b28d2245ee7d22ab2998205b3
Sean Tang <work.seantang@gmail.com>
2023-05-04 18:22:04 -0700

    initial automated testing with selenium and chrome

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -129,6 +129,16 @@ CHANNEL_LAYERS = {
     },
 }
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+    }
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
